fd53576d2c7b1ce4f17ede367ea510c2ba237429
Lidhin Sanjai v <lidhinoffc@gmail.com>
2026-07-03 16:32:06 +0530

    Delete text directory

--- text/analyze.py
+++
@@ -1,8 +0,0 @@
-def word_count(text):
-    return len(text.split())
-
-def char_count(text):
-    return len(text)
-
-def sentence_count(text):
-    return len([s for s in text.split('.') if s.strip()])
